2e6248885d875faa0c8c17ee0b4f09ff756d910a
Sikander Riaz <kahmoshlarka@gmail.com>
2025-08-20 12:37:58 +0500

    feat: add node related stuff in eslint config

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node, //
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn", // warn instead of error
+    },
+  },
+]);
